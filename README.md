7b2a05fc68389a5b3f863a50e97ca0d97779f9f6
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-05-15 10:55:24 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 > _Fork_ deze leertaak en ga aan de slag. Onderstaande outline ga je gedurende deze taak in jouw eigen GitHub omgeving uitwerken. De instructie vind je in: [docs/INSTRUCTIONS.md](docs/INSTRUCTIONS.md)
 
-# Titel
+# WestConnect
 <!-- Geef je project een titel en schrijf in één zin wat het is -->
+WestConnect is een platform waar vraag en aanbod in Amsterdam West samen komt.
+
+![Recording2024-03-21at10 53 131-ezgif com-resize](https://github.com/itsValyria/the-web-is-for-everyone-interactive-functionality/assets/76444716/6b616806-3eef-41c8-be8a-45571208b18e)
 
 ## Inhoudsopgave
 
@@ -9,24 +12,75 @@
   * [Gebruik](#gebruik)
   * [Kenmerken](#kenmerken)
   * [Installatie](#installatie)
-  * [Bronnen](#bronnen)
   * [Licentie](#licentie)
 
 ## Beschrijving
 <!-- Bij Beschrijving staat kort beschreven wat voor project het is en wat je hebt gemaakt -->
 <!-- Voeg een mooie poster visual toe 📸 -->
 <!-- Voeg een link toe naar Github Pages 🌐-->
+De Buurtcampus is een project voor een letterlijke buurt campus waar vraag en aanbod elkaar kruist. Het is een initiatief om mensen in de buurt met elkaar te verbinden. Vrijwilligers gezocht, zoek jij die ene toffe muzikant voor op je buurtfeestje of organiseer je een theekransje voor ouderen? Alles is welkom!
 
 ## Gebruik
 <!-- Bij Gebruik staat de user story, hoe het werkt en wat je er mee kan. -->
 
+De Buurtcampus heeft deze sprint vijf pagina's:
+
+### 1. Initiatieven overzicht page
+Deze pagina is gebaseerd op de volgende user story: <br>
+```Als buurtbewoner van Amsterdam West wil ik een overzicht van alle buurtinitiatieven kunnen bekijken, zodat ik een indruk kan krijgen van wat er allemaal te doen is in de wijk.``` <br>
+en <br>
+```Als buurtbewoner van Amsterdam West wil ik een initiatief kunnen liken.```
+
+### 2. Initiatief informatie page
+Deze pagina is gebaseerd op de volgende user story: <br>
+```Als buurtbewoner van Amsterdam West wil ik meer informatie over een initiatief kunnen bekijken, zodat ik kan zien of ik mij bij het initiatief wil aansluiten.```
+
+### 2. Initiatief aanmelden page
+Deze pagina is gebaseerd op de volgende user story: <br>
+```Als stichting of bewoner van Amsterdam West wil ik een initiatief kunnen plaatsen op de website.```
+
+### 4. Landing page
+Deze pagina is gebaseerd op de volgende user story: <br>
+```Als stichting of bewoner wil ik in een opslag beknopt zien wat de WestConnect is en wat ze doen.```
+
+### 4. FAQ page
+Deze pagina is gebaseerd op de volgende user story: <br>
+```Als stichting of bewoner wil veelgestelde vragen over WestConnect duidelijk kunnen bekijken.```
+
 ## Kenmerken
 <!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met JS gedaan en hoe? Misschien heb je iets met NodeJS gedaan, of heb je een framwork of library gebruikt? -->
+
+In dit project maak ik gebruik van Node, Express, EJS en Progressive Enhancement.
+
+### Node
+Node is een open source platform wat wij in dit geval gebruiken om JavaScript te gebruiken voor server-side werken. De JavaScript wordt in dit geval gelijk gerund in de computer in plaats van in de browser.
+
+### Express
+Express is een framework voor Node. Het is eigenlijk een laagje over Node heen, wat ervoor zorgt dat helpt om servers en routes te managen.
+
+### EJS
+EJS is een template voor Node. Op deze manier kun je je HTML met zo min mogelijk code maken. Zo heb je een ```head``` en ```foot``` die je dan op meerdere pagina's kan implementeren. Ook kun je bepaalde secties loopen. Dit betekent dat je hetzelfde stukje HTML meerdere keren op de website ziet, terwijl het eigenlijk maar één keer in je ```index.ejs``` staat.
+
+### Progressive Enhancement
+Progressive Enhancement betekent eigenlijk dat je werkt in drie fundamentele lagen, zodat je website het altijd zo goed mogelijk doet. De eerste laag is de HTML, de fundamentals. De tweede laag is de CSS, de visuals. En de derde laag zijn de enhancements, vaak met JavaScript of andere poespas.
 
 ## Installatie
 <!-- Bij Instalatie staat hoe een andere developer aan jouw repo kan werken -->
 
-## Bronnen
+### Stap 1
+Installeer [NodeJS](https://nodejs.org/en).
+
+### Stap 2
+Fork deze repository en open het in VScode.
+
+### Stap 3
+Run de command ```npm install```
+
+### Stap 4
+Run de command ```npm start```
+
+### Stap 5
+Volg de link naar localhost en HAPPY CODING!
 
 ## Licentie
 
--- server.js
+++ server.js
@@ -1,1 +1,162 @@
-console.log('Hier komt jullie server')
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+// Importeer slugify voor leesbare URLs met slug
+import slugify from "slugify";
+
+// Declare de base URL van de directus API
+const baseUrl = "https://fdnd-agency.directus.app";
+
+// Maak een nieuwe express app aan
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+
+// Stel de map met ejs templates in
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static('public'))
+
+// Fetch de data van de API
+const fetchFromApi = (endpoint) => {
+  return fetchJson(baseUrl + endpoint).then((response) => response.data);
+};
+
+// Fetch de data van de API van dh_services
+const fetchData = () => {
+  return fetchFromApi("/items/dh_services");
+};
+
+// Data ophalen van de API
+fetchData().then((allAdvertisementsData) => {
+
+  // GET-route voor de indexpagina
+  app.get("/", function (request, response) {
+    response.render("index", { services: allAdvertisementsData });
+  });
+
+  // GET-route voor de FAQ pagina
+  app.get("/faq", function (request, response) {
+    response.render("faq", { services: allAdvertisementsData, });
+  });
+
+  // GET-route voor de overzichtspagina
+  app.get("/overzicht", function (request, response) {
+    response.render("overzicht", { services: allAdvertisementsData });
+  });
+
+  // GET-route voor de overzicht detail page van een service met slug
+  app.get("/service/:slug", function (request, response) {
+    const serviceSlug = request.params.slug;
+    const service = allAdvertisementsData.find(
+      (service) => service.slug === serviceSlug
+    );
+    if (!service) {
+      console.error(`Service met slug ${serviceSlug} niet gevonden`);
+      response.status(404).send("Service niet gevonden");
+      return;
+    }
+    response.render("service", { service: service });
+  });
+
+  // Zorg voor een leesbare URL door de titel weer te geven in plaats van het ID
+  allAdvertisementsData.forEach((service) => {
+    service.slug = slugify(service.title, { lower: true });
+  });
+
+  // GET-route voor de pagina om een service aan te melden
+  app.get("/service-aanmelden", function (request, response) {
+    response.render("service-aanmelden", { services: allAdvertisementsData });
+  });
+
+  // GET-route voor de pagina om een service aanmelding succes weer te geven
+  app.get("/service-aanmelden-gelukt", function (request, response) {
+    response.render("service-aanmelden-gelukt", { services: allAdvertisementsData });
+  });
+
+  // POST-route om formuliergegevens te verwerken
+  app.post("/service-aanmelden", function (request, response) {
+    const formData = request.body;
+
+    const newAdvertisement = {
+      name: formData.name,
+      surname: formData.surname,
+      email: formData.email,
+      contact: formData.contact,
+      title: formData.title,
+      short_description: formData.short_description,
+      long_description: formData.long_description,
+      location: formData.location,
+      neighbourhood: formData.neighbourhood,
+      start_date: formData.start_date,
+      end_date: formData.end_date,
+      start_time: formData.start_time,
+      end_time: formData.end_time,
+    };
+
+    // Gegevens naar de API endpoint sturen
+    fetchJson(baseUrl + "/items/dh_services", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newAdvertisement),
+    }).then((responseFromAPI) => {
+      console.log("Response from API:", responseFromAPI);
+
+      // Bijwerken van de gegevens vanuit de API
+      fetchData().then((updatedData) => {
+        allAdvertisementsData = updatedData;
+        response.redirect("/service-aanmelden-gelukt");
+      }).catch((error) => {
+        console.error("Error fetching data from API:", error);
+        response.status(500).send("Internal Server Error");
+      });
+    }).catch((error) => {
+      console.error("Error while posting data to API:", error);
+      response.status(500).send("Internal Server Error");
+    });
+  });
+
+  // POST-route voor het liken van een service
+  app.post("/like", function (request, response) {
+    const { like_id } = request.body;
+    console.log("Like verzoek voor service met ID:", like_id);
+    const service = allAdvertisementsData.find(
+      (service) => service.id === parseInt(like_id)
+    );
+    if (service) {
+      service.likes = (service.likes || 0) + 1;
+      // Aantal likes bijwerken in de Directus API
+      fetchJson(baseUrl + `/items/dh_services/${like_id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ likes: service.likes }),
+      }).then(() => {
+        console.log("Aantal likes bijgewerkt voor service:", service);
+      }).catch((error) => {
+        console.error("Error patching likes in Directus API:", error);
+      });
+    } else {
+      console.log("Service niet gevonden voor ID:", like_id);
+      response.status(404).send("Service niet gevonden");
+    }
+  });
+
+  // Poort instellen waarop Express moet luisteren
+  app.set("port", process.env.PORT || 8000);
+
+  // Start de Express server
+  app.listen(app.get("port"), function () {
+    console.log(`Applicatie gestart op http://localhost:${app.get("port")}`);
+  });
+});
